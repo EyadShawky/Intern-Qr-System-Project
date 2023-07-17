48c245fd7a7ea182c8b0b0a95e1ef6130d299dbc
EyadShawky <eyadshawky0@gmail.com>
2023-07-17 13:15:51 +0300

    admin route enhancment

--- intern2023/routes/web.php
+++ intern2023/routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/' , [UserController::class , 'index']);
 Route::post('/' , [UserController::class , 'store']);
-Route::get('/pdRkAAT+XxepOb8drasiSw==' , [adminController::class , 'index']);
+Route::get('/admin/pdRkAAT+XxepOb8drasiSw==' , [adminController::class , 'index']);
 
