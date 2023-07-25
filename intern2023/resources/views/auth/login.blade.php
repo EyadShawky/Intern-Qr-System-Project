5545326ebc88a7a6473b19326842baa105ed98b7
EyadShawky <eyadshawky0@gmail.com>
2023-07-25 12:46:49 +0300

    test

--- intern2023/resources/views/auth/login.blade.php
+++ intern2023/resources/views/auth/login.blade.php
@@ -5,26 +5,29 @@ Tatweer Misr | Login Admin
 @endsection
 
 @section('content')
-    @guest()
-    <form action="{{url('/admin/pdRkAAT+XxepOb8drasiSw==/login')}}" method="post" enctype="multipart/form-data">
+@guest()
+<form action="{{url('/admin/pdRkAAT+XxepOb8drasiSw==/login')}}" method="post" enctype="multipart/form-data">
     @csrf
-        <div class="container">
-            <div class="input-group mb-3 w-75 m-auto">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
-                </div>
-                <input name="email" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    <div class="container">
+        <div class="input-group mb-3 w-75 m-auto">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
             </div>
+            <input name="email" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            @if($errors->has('email'))
+             <small class="form-text invalid-feedback">{{$errors->first('email')}}</small>
+            @endif
+        </div>
 
 
-            <div class="input-group mb-3 w-75 m-auto">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
-                </div>
-                <input name="password" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <div class="input-group mb-3 w-75 m-auto">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
             </div>
-                
-           <input type="submit" value="Login" class="btn button-style  btn-danger m-auto">
+            <input name="password" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
+
+        <input type="submit" value="Login" class="btn button-style  btn-danger m-auto">
     </div>
 </form>
 @endguest
--- intern2023/routes/web.php
+++ intern2023/routes/web.php
@@ -19,20 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/' , [UserController::class , 'index']);
-Route::post('/' , [UserController::class , 'store']);
-Route::get('/admin/pdRkAAT+XxepOb8drasiSw==' , [adminController::class , 'index']);
-Route::get('/admin/pdRkAAT+XxepOb8drasiSw==/qr' , [adminController::class , 'adminQR']);
-Route::get('/qr' , [userCodeController::class , 'qrCode'])->name('Home.qr');
-Route::post('/user-code' , [userCodeController::class , 'store'])->name('user-code.store');
-Route::get('/export/{format}', 'ExportController@export')->name('export.download');
-Route::get('/export' , [exportControllerTwo::class , 'index']);
-Route::get('/', [UserController::class, 'index'])->name('form');
-Route::post('/', [UserController::class, 'store']);
-Route::get('/qr', [userCodeController::class, 'qrCode'])->name('Home.qr');
-Route::post('/user-code', [userCodeController::class, 'store'])->name('user-code.store');
-Route::get('/export/{format}', 'ExportController@ExportController')->name('export.download');
-
 Route::get('/', [UserController::class, 'index'])->name('form');
 Route::post('/', [UserController::class, 'store']);
 Route::get('/qr', [userCodeController::class, 'qrCode'])->name('Home.qr');
