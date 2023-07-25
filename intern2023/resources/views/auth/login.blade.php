a0a741e785263463f3362ee4beddacf3a8b47a5b
EyadShawky <eyadshawky0@gmail.com>
2023-07-25 13:01:41 +0300

    test

--- intern2023/resources/views/auth/login.blade.php
+++ intern2023/resources/views/auth/login.blade.php
@@ -14,9 +14,6 @@ Tatweer Misr | Login Admin
                 <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
             </div>
             <input name="email" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            @if($errors->has('email'))
-             <small class="form-text invalid-feedback">{{$errors->first('email')}}</small>
-            @endif
         </div>
 
         <div class="input-group mb-3 w-75 m-auto">
@@ -25,7 +22,6 @@ Tatweer Misr | Login Admin
             </div>
             <input name="password" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
-
         <input type="submit" value="Login" class="btn button-style  btn-danger m-auto">
     </div>
 </form>
